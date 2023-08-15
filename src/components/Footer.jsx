db4f2a45e39eab5cca136468f59c7972911fc95c
Andrés Del Cerro <alumno.254958@ies-azarquiel.es>
2023-08-15 17:21:23 +0200

    Updated footer height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 h-screen">
       <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © {new Date().getFullYear()}{" "}
